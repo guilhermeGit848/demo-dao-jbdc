a2675192b4f134128d7f8e950e701725211953c2
Guilherme <mendesgui43@gmail.com>
2026-07-02 18:03:28 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,11 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoJBDC;
+
 public class DaoFactory {
 
+	public static SellerDao createSellerDao () {
+		return new SellerDaoJBDC(DB.getConnection());
+	}
 }
